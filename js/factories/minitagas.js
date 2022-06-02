29593a7ccf4f8425ae9e6deb13899176e3708c3c
alaodriss <alaodriss@gmail.com>
2022-06-02 11:17:41 +0200

    push tags in arry

--- js/factories/minitagas.js
+++ js/factories/minitagas.js
@@ -1,20 +1,20 @@
-// function minitagsDOM(value, color, type) {
-//     const wrapper = document.createElement("button");
+function minitagsDOM(value, color, type) {
+    const wrapper = document.createElement("button");
   
-//     wrapper.setAttribute("type", "button");
-//     wrapper.setAttribute("datavalue", `${type}`);
-//     wrapper.classList.add("btn", `btn-${color}`, "text-white", "btn-sm");
-//     wrapper.classList.add("ps-3", "py-2");
-//     wrapper.classList.add("tag-button");
+    wrapper.setAttribute("type", "button");
+    wrapper.setAttribute("datavalue", `${type}`);
+    wrapper.classList.add("btn", `btn-${color}`, "text-white", "btn-sm");
+    wrapper.classList.add("ps-3", "py-2");
+    wrapper.classList.add("tag-button");
   
-//     let miniTagCard = "";
-//     miniTagCard += `
+    let miniTagCard = "";
+    miniTagCard += `
     
-//     ${value.charAt(0).toUpperCase() + value.slice(1)}
-//     <i class="far fa-times-circle fa-lg close-tag"></i>
+    ${value.charAt(0).toUpperCase() + value.slice(1)}
+    <i class="far fa-times-circle fa-lg close-tag"></i>
   
-//       `;
-//     wrapper.innerHTML = miniTagCard;
-//     return wrapper;
-//   }
+      `;
+    wrapper.innerHTML = miniTagCard;
+    return wrapper;
+  }
   

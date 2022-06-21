0dd5c294ec1b7151b2900640728ef026d5c7edf0
alaodriss <alaodriss@gmail.com>
2022-06-21 14:12:26 +0200

    algosOne

--- js/tools/removeminitags.js
+++
@@ -1,9 +0,0 @@
-function removeMiniTag() {
-  const removeMiniTag = document.querySelectorAll(".tag-button");
-  if (removeMiniTag.length) {
-    for (let j = 0; j < removeMiniTag.length; j++)
-      removeMiniTag[j].addEventListener("click", () => {
-        removeMiniTag[j].remove();
-      });
-  }
-}
